62d488e445b1529f183ede24fbdd8fd8340645db
Efe Özen <111181897+efezen@users.noreply.github.com>
2023-01-05 17:51:28 +0300

    finish project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import './App.css';
+import youtube from "./api/youtube";
+import { Grid } from "@mui/material";
+import SearchBar from "./components/SearchBar";
+import { useState } from "react";
+import VideoDetail from "./components/VideoDetail";
+import VideoList from "./components/VideoList";
+const App = () => {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState({ id: {}, snippet: {} });
 
-function App() {
   return (
-    <div className="App">
-      <h1>hello world.</h1>
-    </div>
+    <Grid style={{ justifyContent: "center" }} container spacing={10}>
+      <Grid item xs={11}>
+        <Grid container spacing={10}>
+          <Grid item xs={12}>
+            <SearchBar onSubmit={handleSubmit} />
+          </Grid>
+          <Grid item xs={8}>
+            <VideoDetail video={selectedVideo} />
+          </Grid>
+          <Grid item xs={4}>
+            <VideoList videos={videos} onVideoSelect={setSelectedVideo} />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
-}
+
+  async function handleSubmit(searchTerm) {
+    const {
+      data: { items: videos },
+    } = await youtube.get("search", {
+      params: {
+        part: "snippet",
+        maxResults: 5,
+        key: "YouTube_Data_API_v3",
+        q: searchTerm,
+      },
+    });
+
+    setVideos(videos);
+    setSelectedVideo(videos[0]);
+  }
+};
 
 export default App;
